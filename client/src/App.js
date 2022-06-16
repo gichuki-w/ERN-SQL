d1d2b67c791ba3931e18fcf400d721f702e1aa4c
gichuki-w <gichukiwakahu@gmail.com>
2022-06-16 16:28:40 +0300

    sql connection

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <label>Name</label>
+      <input type="text" />
+      <label>Age</label>
+      <input type="number" />
+      <label>Country</label>
+      <input type="text" />
+      <label>Wage</label>
+      <input type="number" />
+      <label>Shool</label>
+      <input type="text" />
+    </div>
+  );
+}
+
+export default App;
